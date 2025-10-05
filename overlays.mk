9c9bb639c1a448e896175391da1e34e5209e2e26
kcl <lh103100@outlook.com>
2025-10-05 22:57:59 +0800

    Font: Add Ubuntu Mono Sans [1/2]

--- overlays.mk
+++ overlays.mk
@@ -56,6 +56,7 @@ PRODUCT_PACKAGES += \
     FontStoropiaSourceOverlay \
     FontRobotoFlexRegularOverlay \
     FontGoogleSansFlexRegularOverlay \
+    FontUbuntuMonoOverlay \
     FontUbuntuOverlay
 
 # Icon shapes
